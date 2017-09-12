cad54f8f5f1a0bd4a5a38425afb0cd132254cb56
俊臣 王 <wjc1380007@outlook.com>
2017-09-12 17:16:08 +0800

    add dominance_tree
    with simple_test

---
+++ test/test_dominanceGraph.cpp
@@ -0,0 +1,19 @@
+#define BOOST_TEST_MODULE dominanceGraph
+#include <boost/test/unit_test.hpp>
+
+#include "../utility/dominance_graph/dominance_graph.h"
+
+BOOST_AUTO_TEST_SUITE(dominanceGraph_test)
+
+using namespace dominance_graph;
+
+BOOST_AUTO_TEST_CASE(test_case1) {
+	
+	vector < vector<double> > data = { { 6,2 },{ 1,2 },{ 0,5 },{ 1,4, },{ 0,7 },{ 1,6 },{ 2,3 },{ 5,0 },{ 4,4 },{ 3,2 },{ 1,1 },{ 3,6 },{ 0,3 },{ 4,1 },{ 4,7 },{ 5,6 },{ 6,4 },{ 6,7 } };
+	graph test_graph(data);
+	test_graph.quick_sort();
+	test_graph.set_ranking();
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()
