761a5c8bc4d25c268a8a37deafaccc034bcd4600
Changhe Li <changhe.lw@gmail.com>
2017-10-15 10:35:27 +0800

    set_opt_mode added

--- core/problem/problem.h
+++ core/problem/problem.h
@@ -136,6 +136,11 @@ namespace OFEC {
 		size_t total_evaluations() {
 			return m_total_eval;
 		}
+
+		void set_opt_mode(optimization_mode m, size_t idx=0) {
+			m_opt_mode[idx]=m;
+		}
+
 	protected:
 		problem& operator=(const problem& rhs);  // assignment is not allowed outside
 		problem& operator=(problem&& rhs);
