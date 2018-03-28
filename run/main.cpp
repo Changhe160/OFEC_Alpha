99c4f6001aaab8796b755dddd22b5d62aafabc79
Changhe Li <changhe.lw@gmail.com>
2018-03-28 08:53:25 +0800

    unit test added

--- run/main.cpp
+++ run/main.cpp
@@ -7,15 +7,19 @@ int main(int argc, char* argv[]) {
 
 	time_t timer_start, timer_end;
 
-	#ifndef OFEC_UNIT_TEST
-		OFEC::set_global_parameters(argc, argv);
-	#endif // !OFEC_UNIT_TEST
+#ifndef OFEC_UNIT_TEST
+	OFEC::set_global_parameters(argc, argv);
+#endif // !OFEC_UNIT_TEST
 
 	OFEC::register_problem();
 	OFEC::register_algoritm();
 	time(&timer_start);
-	//OFEC::run();
+
+#ifdef OFEC_UNIT_TEST
 	int result = Catch::Session().run(argc, argv);
+#else
+	OFEC::run();
+#endif // OFEC_UNIT_TEST
 
 	time(&timer_end);
 	std::cout << "Time used: " << difftime(timer_end, timer_start) << " seconds" << std::endl;
