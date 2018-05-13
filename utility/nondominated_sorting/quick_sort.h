b85460dad77d6684fa67606567ba947c3fd61c9c
Changhe Li <changhe.lw@gmail.com>
2018-05-13 15:34:49 +0800

    quick_sort of one dimensional data provided

--- utility/nondominated_sorting/quick_sort.h
+++ utility/nondominated_sorting/quick_sort.h
@@ -2,7 +2,8 @@
 #define NDS_QUICK_SORT_H
 
 #include <vector>
-
+#include <thread>
+#include <memory> 
 namespace NS {
 
 	template<typename T>
@@ -20,7 +21,7 @@ namespace NS {
 	}
 
 	template<typename T>
-	int Partition(const std::vector<std::vector<T>>& data, std::vector<int>& A, const int obj_idx, int low, int high, int& Noc)
+	int partition(const std::vector<std::vector<T>>& data, std::vector<int>& A, const int obj_idx, int low, int high, int& Noc)
 	{
 		int pivot = A[low];
 		while (low < high)
@@ -44,8 +45,8 @@ namespace NS {
 	{
 		if (low < high) //递归跳出条件
 		{
-			//Partition就是上面的划分操作
-			int pivot = Partition(data, A, obj_idx, low, high, Noc); //划分
+			//partition就是上面的划分操作
+			int pivot = partition(data, A, obj_idx, low, high, Noc); //划分
 
 			QuickSort(data, A, obj_idx, low, pivot - 1, Noc); //左半部分递归
 
@@ -74,6 +75,95 @@ namespace NS {
 		}
 		return Noc;
 	}
+
+	template<class T>
+	bool compare(const T &d1, const T &d2, bool ascending = true) {
+		if (ascending) {
+			if (d1<d2) return true;
+			else return false;
+		}
+		else {
+			if (d1<d2) return false;
+			else return true;
+		}
+	}
+
+	template<class T>
+	bool compare(T *d1, T *d2, bool ascending = true) {
+		if (ascending) {
+			if (*d1<*d2) return true;
+			else return false;
+		}
+		else {
+			if (*d1<*d2) return false;
+			else return true;
+		}
+	}
+
+	template<class T>
+	void quick_sort(const T &data, int size, std::vector<int>& index, bool ascending = true, int low = 0, int up = -1, int num = -1,bool first=true) {
+		//sort data from small to large, and put the order in index
+		//size: the size of data  
+		//low, up : the range of data to be sorted
+		//num : the max/min number of data within low and up 
+		static thread_local std::unique_ptr<int> lb;
+		static thread_local std::unique_ptr<std::vector<bool>> flag;
+		if (first)	{
+			if (up == -1) up = size - 1;
+			if (num == -1) num = size;
+			flag.reset(new std::vector<bool>(num, false));
+			lb.reset(new int(low));
+			if (index.size() == 0 || index.size() != size)		
+				index.resize(size);
+			for (auto i = index.begin(); i != index.end(); ++i) 
+				*i = i - index.begin();
+		}
+		if (low >= up) return;
+		int i = 0;
+		for (; i<num; i++) {
+			if ((*flag.get())[i] == false)	
+				break;
+		}
+		if (i == num) return;
+		int left = low + 1;
+		int right = up;
+		int pivot = low;
+
+		while (left<right) {
+			while (compare(data[index[left]], data[index[pivot]], ascending) && left<right)
+				left++;
+			while (!compare(data[index[right]], data[index[pivot]], ascending) && left<right)
+				right--;
+			if (left<right) {
+				int t = index[left];
+				index[left] = index[right];
+				index[right] = t;
+			}
+		}
+		while (!compare(data[index[left]], data[index[pivot]], ascending) && left>pivot)
+			left--;
+		if (compare(data[index[left]], data[index[pivot]], ascending)) {
+			int t = index[left];
+			index[left] = index[pivot];
+			index[pivot] = t;
+			if (left - *lb<num)
+				(*flag.get())[left - *lb] = true;
+		}
+		else  {
+			if (pivot - *lb<num)
+				(*flag.get())[pivot - *lb] = true;
+		}
+		i = 0;
+		for (; i<num; i++) {
+			if ((*flag.get())[i] == false)		break;
+		}
+		if (i == num) return;
+
+		pivot = left;
+		quick_sort(data, pivot - low, index, ascending, low, pivot - 1, num, false);
+		quick_sort(data, up - pivot, index, ascending, pivot + 1, up, num, false);
+	}
+
 }
 
 #endif // !NDS_QUICK_SORT_H
